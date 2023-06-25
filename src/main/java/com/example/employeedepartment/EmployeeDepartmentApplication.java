d92a432feeb9a8e11039959472ec4dc9cf6f9c77
17152 <nazarovtolya2@gmail.com>
2023-06-25 17:17:41 +0500

    Начало ДЗ

---
+++ src/main/java/com/example/employeedepartment/EmployeeDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDepartmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeDepartmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeedepartment/EmployeeDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeedepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDepartmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
